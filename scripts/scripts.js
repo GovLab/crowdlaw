cbcbbfacb4b0dc5c68422a9723a91088f862573f
mocxd <mark.adkins.hastings@gmail.com>
2018-07-14 05:35:54 -0400

    fixes modal not closing properly

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -66,6 +66,9 @@ $(document).ready(function () {
     $('#overlay').click(function () {
         $('.js-active').removeClass('js-active');
         $('.js-active-menu').removeClass('js-active-menu');
+        $('.js-target-modal').removeClass('js-active');
+        $('#overlay').removeClass('js-active');
+        $('body').removeClass('js-body-modal-active');
     });
 
     // Slider
